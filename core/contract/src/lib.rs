ebdd5ee59f4bfa29f1b792c109e3098dc412dc5b
weiler1 <dotweilerofficial@gmail.com>
2022-12-13 22:41:24 +0530

    added priliminary unit tests

---
+++ core/contract/src/lib.rs
@@ -0,0 +1,252 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, log, near_bindgen, AccountId};
+//use near_sdk::collections::{UnorderedMap};
+use std::collections::BTreeMap;
+
+//use phpify::array::array_unshift;
+
+mod task;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+  pub task_count: u128,
+  pub clients: BTreeMap<u128, AccountId>,
+  pub quotes: BTreeMap<u128, u128>,
+  pub applicants: BTreeMap<u128, Vec<AccountId>>,
+  pub assignees: BTreeMap<u128, AccountId>,
+}
+
+impl Default for Contract {
+  fn default() -> Self {
+    Self{
+      task_count: 0,
+      clients: BTreeMap::new(),
+      quotes: BTreeMap::new(),
+      applicants: BTreeMap::new(),
+      assignees: BTreeMap::new(),
+    }
+  }
+}
+
+#[near_bindgen]
+impl Contract {
+  #[init]
+  #[private] // Public - but only callable by env::current_account_id()
+  pub fn init() -> Self {
+    assert!(!env::state_exists(), "Already initialized");
+    Self {
+        task_count: 0,
+        clients: BTreeMap::new(),
+        quotes: BTreeMap::new(),
+        applicants: BTreeMap::new(),
+        assignees: BTreeMap::new(),
+    }
+  }
+
+  // Public - client list getter
+  pub fn get_client_list(&self) -> BTreeMap<u128, AccountId> {
+    self.clients.clone()
+  }
+
+  // Public - quote list getter
+  pub fn get_quote_list(&self) -> BTreeMap<u128, u128> {
+    self.quotes.clone()
+  }
+
+  // Public - assignee list getter
+  pub fn get_assignee_list(&self) -> BTreeMap<u128, AccountId> {
+    self.assignees.clone()
+  }
+
+  pub fn get_task_count(&self) -> u128{
+    self.task_count.clone()
+
+  }
+  
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use near_sdk::testing_env;
+  use near_sdk::test_utils::VMContextBuilder;
+  use near_sdk::Balance;
+
+  //const BENEFICIARY: &str = "beneficiary";
+  const NEAR: u128 = 1000000000000000000000000;
+
+  #[test]
+  fn initializes() {
+      let contract = Contract::init();
+      assert_eq!(contract.task_count, 0);
+  }
+
+  #[test]
+  fn check_quote() {
+      let mut contract = Contract::init();
+
+      // Make a donation
+      set_context("client_a", 1*NEAR);
+      contract.setup_task(NEAR*3);
+      let first_quote = contract.get_quote_for_task(contract.task_count);
+
+      log!("{} tasks", contract.task_count);
+      // Check the donation was recorded correctly
+      //assert_eq!(contract.task_count, 1);
+      assert_eq!(first_quote, NEAR*3,"assertion failed");
+
+      
+  }
+
+  #[test]
+  fn check_client() {
+      let mut contract = Contract::init();
+
+      // Make a donation
+      set_context("client_a", 1*NEAR);
+      contract.setup_task(NEAR*3);
+      let first_client = contract.get_client_for_task(contract.task_count);
+
+      log!("{} client", contract.get_client_for_task(contract.task_count));
+      // Check the donation was recorded correctly
+      //assert_eq!(contract.task_count, 1);
+      assert_eq!(first_client, AccountId::new_unchecked("client_a".to_string()),"assertion failed 2");
+
+      
+  }
+
+  #[test]
+  fn check_task_count() {
+      let mut contract = Contract::init();
+
+      // Make a donation
+      set_context("client_b", 1*NEAR);
+      contract.setup_task(NEAR*2);
+      //let first_quote = contract.get_quote_for_task(contract.task_count);
+
+      log!("{} tasks", contract.task_count);
+      // Check the donation was recorded correctly
+      assert_eq!(contract.task_count, 1);
+      //assert_eq!(first_quote, NEAR*3,"assertion failed");
+
+      
+  }
+
+  #[test]
+  fn check_application() {
+      let mut contract = Contract::init();
+
+      // Make a donation
+      set_context("client_a", 1*NEAR);
+      contract.setup_task(NEAR*5);
+      //let first_quote = contract.get_quote_for_task(contract.task_count);
+
+      set_context("client_b", 1*NEAR);
+      let res = contract.apply_for_task(1);
+
+      //log!("{} tasks", contract.task_count);
+      // Check the donation was recorded correctly
+      //assert_eq!(contract.task_count, 1);
+      assert_eq!(true, res,"assertion failed 3");
+
+      
+  }
+
+  #[test]
+  fn check_applicant() {
+      let mut contract = Contract::init();
+
+      // Make a donation
+      set_context("client_a", 1*NEAR);
+      contract.setup_task(NEAR*5);
+      //let first_quote = contract.get_quote_for_task(contract.task_count);
+
+      set_context("client_b", 1*NEAR);
+      let _res = contract.apply_for_task(1);
+
+      let applicant_list = contract.get_applicants_for_task(contract.task_count);
+
+      //log!("{} tasks", contract.task_count);
+      // Check the donation was recorded correctly
+      //assert_eq!(contract.task_count, 1);
+      assert_eq!(applicant_list[0], AccountId::new_unchecked("client_b".to_string()));
+
+      
+  }
+
+  #[test]
+  fn check_assignee() {
+      let mut contract = Contract::init();
+
+      // Make a donation
+      set_context("client_a", 1*NEAR);
+      contract.setup_task(NEAR*5);
+      //let first_quote = contract.get_quote_for_task(contract.task_count);
+
+      set_context("client_b", 1*NEAR);
+      let _res1 = contract.apply_for_task(1);
+
+      set_context("client_c", 1*NEAR);
+      let _res2 = contract.apply_for_task(1);
+
+      let applicant_list = contract.get_applicants_for_task(contract.task_count);
+      let chosen_one = &applicant_list[1];
+      set_context("client_a", 6*NEAR);
+      let _res = contract.assign_task(1, chosen_one.clone());
+      //log!("{} tasks", contract.task_count);
+      // Check the donation was recorded correctly
+      //assert_eq!(contract.task_count, 1);
+      assert_eq!(chosen_one.clone(), AccountId::new_unchecked("client_c".to_string()), "Assertion Failed 5");
+
+      
+  }
+
+  #[test]
+  fn check_validation() {
+      let mut contract = Contract::init();
+
+      // Make a donation
+      set_context("client_a", 1*NEAR);
+      contract.setup_task(NEAR*5);
+      //let first_quote = contract.get_quote_for_task(contract.task_count);
+
+      set_context("client_b", 1*NEAR);
+      let _res1 = contract.apply_for_task(1);
+
+      set_context("client_c", 1*NEAR);
+      let _res2 = contract.apply_for_task(1);
+
+      let applicant_list = contract.get_applicants_for_task(contract.task_count);
+      let chosen_one = &applicant_list[1];
+      set_context("client_a", 6*NEAR);
+      let _res3 = contract.assign_task(1, chosen_one.clone());
+      //log!("{} tasks", contract.task_count);
+      // Check the donation was recorded correctly
+      //assert_eq!(contract.task_count, 1);
+      let caller = env::current_account_id();
+      set_context_2(caller, 10*NEAR);
+      let res = contract.validate_task(1, true);
+
+      assert_eq!(res, true, "Assertion Failed 6");
+
+      
+  }
+
+  // Auxiliar fn: create a mock context
+  fn set_context(predecessor: &str, amount: Balance) {
+    let mut builder = VMContextBuilder::new();
+    builder.predecessor_account_id(predecessor.parse().unwrap());
+    builder.attached_deposit(amount);
+
+    testing_env!(builder.build());
+  }
+
+  fn set_context_2(predecessor: AccountId, amount: Balance) {
+    let mut builder = VMContextBuilder::new();
+    builder.predecessor_account_id(predecessor);
+    builder.attached_deposit(amount);
+
+    testing_env!(builder.build());
+  }
+}
---
+++ core/contract/src/task.rs
@@ -0,0 +1,149 @@
+use crate::Contract;
+use crate::ContractExt;
+
+use near_sdk::serde::Serialize;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, log, near_bindgen, AccountId, Promise, Balance};
+use near_sdk::json_types::U128;
+//use phpify::array::array_unshift;
+
+pub const STORAGE_COST: u128 = 1_000_000_000_000_000_000_000;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Task {
+  pub account_id: AccountId, 
+  pub payable_amount: U128,
+  pub task_id: u128,
+}
+
+#[near_bindgen]
+impl Contract {
+  #[payable] // Public - People can attach money
+  pub fn setup_task(&mut self, task_payment: u128) -> u128 {
+    // Get who is calling the method and how much $NEAR they attached
+    let client: AccountId = env::predecessor_account_id();
+    let setup_fee: Balance = env::attached_deposit();
+
+    //let mut donated_so_far = self.donations.get(&donor).unwrap_or(0);
+
+    assert!(setup_fee > STORAGE_COST, "Attach at least {} yoctoNEAR", STORAGE_COST);
+    assert!(task_payment > STORAGE_COST* 10, "Too Little Being Offered");
+    
+    
+    let new_task_id: u128 = self.get_task_count() + 1;
+    self.task_count = new_task_id;
+    self.quotes.insert(new_task_id, task_payment);
+    self.clients.insert(new_task_id, client.clone());
+    self.applicants.insert(new_task_id, Vec::new());
+    
+    log!("Thank you {} for creating a task worth {}!", client.clone(), task_payment);
+    
+    log!("Showing first quote: {}", self.quotes.get(&new_task_id).unwrap());
+
+    log!("Showing first client: {}", self.clients.get(&new_task_id).unwrap());
+
+    // Return the total amount donated so far
+    return new_task_id;
+  }
+
+  #[payable] // Public - People can attach money
+  pub fn apply_for_task(&mut self, task_id: u128) -> bool {
+    // Get who is calling the method and how much $NEAR they attached
+    let applicant: AccountId = env::predecessor_account_id();
+
+    assert!(&applicant != self.clients.get(&task_id).unwrap(), "You CANNOT apply for your own task!");
+    
+    let application_fee: Balance = env::attached_deposit();
+
+    //let mut donated_so_far = self.donations.get(&donor).unwrap_or(0);
+
+    assert!(application_fee > STORAGE_COST, "Attach at least {} yoctoNEAR", STORAGE_COST);
+    
+    
+    
+    
+    let mut applicant_vec = self.applicants.get(&task_id).unwrap().clone();
+
+    //array_unshift(&mut applicant_vec, &applicant);
+
+    applicant_vec.push(applicant.clone());
+
+    self.applicants.remove(&task_id);
+    self.applicants.insert(task_id, (*applicant_vec).to_vec());
+    
+    
+    log!("Thank you for applying!");
+    
+    
+
+    // Return the total amount donated so far
+    return true;
+  }
+
+  #[payable] // Public - People can attach money
+  pub fn assign_task(&mut self, task_id: u128, assignee: AccountId) -> bool {
+    // Get who is calling the method and how much $NEAR they attached
+    let caller: AccountId = env::predecessor_account_id();
+    assert!(&caller == self.clients.get(&task_id).unwrap());
+
+    let pre_payment: Balance = env::attached_deposit();
+
+    //let mut donated_so_far = self.donations.get(&donor).unwrap_or(0);
+
+    assert!(&pre_payment > self.quotes.get(&task_id).unwrap(), "Not enough to cover the quoted price: {}", self.quotes.get(&task_id).unwrap());
+    
+    self.assignees.insert(task_id, assignee.clone());
+
+    log!("Thank you for choosing {} as your assignee", assignee.clone());
+    
+    return true;
+  }
+
+  #[payable] // Public - People can attach money
+  pub fn validate_task(&mut self, task_id: u128, judgement: bool) -> bool {
+    // Get who is calling the method and how much $NEAR they attached
+    let caller: AccountId = env::predecessor_account_id();
+    //let setup_fee: Balance = env::attached_deposit();
+
+    assert!(caller == env::current_account_id(),"You are not authorized to call this method");
+
+    if judgement{
+        Promise::new(self.assignees.get(&task_id).unwrap().clone()).transfer(*self.quotes.get(&task_id).unwrap());
+        let _r1 = self.clients.remove(&task_id);
+        let _r2 = self.quotes.remove(&task_id);
+        let _r3 = self.assignees.remove(&task_id);
+    }
+    else {
+        Promise::new(self.clients.get(&task_id).unwrap().clone()).transfer(*self.quotes.get(&task_id).unwrap());
+        let _r1 = self.clients.remove(&task_id);
+        let _r2 = self.quotes.remove(&task_id);
+        let _r3 = self.assignees.remove(&task_id);
+    }
+    
+    
+    log!("Task Closed successfully with status: {}", judgement);
+    
+    
+    return true;
+  }
+
+  // Public - get donation by account ID
+  pub fn get_client_for_task(&self, task_id: u128) -> AccountId {
+    return self.clients.get(&task_id).unwrap().clone();
+  }
+
+  pub fn get_assignee_for_task(&self, task_id: u128) -> AccountId {
+    return self.assignees.get(&task_id).unwrap().clone();
+  }
+
+  pub fn get_quote_for_task(&self, task_id: u128) -> u128 {
+    return self.quotes.get(&task_id).unwrap().clone();
+  }
+
+  pub fn get_applicants_for_task(&self, task_id: u128) -> Vec<AccountId> {
+    return self.applicants.get(&task_id).unwrap().clone();
+  }
+
+}
